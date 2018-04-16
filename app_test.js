57c0e93cdceb7f1f476d7c684b25e5bab6ee7d95
heraldohuerta <heraldo.huerta@gmail.com>
2018-04-15 22:46:26 -0300

    JavaScript

---
+++ app_test.js
@@ -0,0 +1,19 @@
+function sumar(a,b) {
+  return (a + b)
+}
+
+var sumar = function(a,b) {
+  return (a + b )
+}
+
+
+
+var sumar = function sumar(a,b) {
+  return (a + b )
+}
+console.log(sumar(1,2));
+
+var arr = [1,'hola',false,32,'A',true];
+for (var i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+};
